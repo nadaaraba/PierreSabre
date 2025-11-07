f0ee393062d8f15419935e47430b301699bce8c9
Nada Araba <rbn5291a@ensfsi.onmicrosoft.com>
2025-11-07 09:14:33 +0100

    creation de la classe humain avec test

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -1,9 +1,16 @@
 package histoire;
 
+import personnages.Humain;
+
 public class HistoireTP4 {
 
 	public static void main(String[] args) {
-
+		Humain prof = new Humain("Prof", "Kombucha", 54);
+		prof.direBonjour();
+		prof.acheter("Kombucha", 12);
+		prof.boire();
+		prof.acheter("jeu", 2);
+		prof.acheter("kimono", 50);
 	}
 
 }
